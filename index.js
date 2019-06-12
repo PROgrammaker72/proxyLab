c99879c0c875a9f0c451e1208a0d8a4a32af0604
Oleg Diordiychuk <o.o.diordiy@gmail.com>
2019-06-12 08:59:19 +0300

    [+]proxy

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 'use strict';
 
-console.log('hello m');
+const http = require('http');
+
+const PORT = 3001;
+
+const proxy = http.createServer((req, res) => {
+  const headers = {
+    'Content-Type': 'text/html',
+  };
+
+  const options = {
+    hostname: req.headers.host.replace(':3001', ''),
+    port: 3002,
+    method: req.method,
+    headers,
+  };
+
+  console.log(options);
+  const mainReq = http.request(options, (mainRes) => {
+    let fullData = '';
+    mainRes.on('data', (chunk) => {
+      fullData += chunk;
+    });
+
+    mainRes.on('end', () => {
+      res.end(fullData);
+    });
+  });
+
+  mainReq.on('error', (e) => {
+    console.error(`problem with request: ${e.message}`);
+  });
+
+  mainReq.end();
+});
+
+proxy.listen(PORT);
